09d70e6825669d7c640719da365ed7f9f7057f5e
Amosatel <amkimotho68@gmail.com>
2023-03-16 18:41:28 -0700

    hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - this is the starting point for program to  be executed
+ *
+ * Return: ends the execution of a function hence 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
