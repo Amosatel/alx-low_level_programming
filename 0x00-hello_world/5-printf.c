484392d5f47bff17941d0152a1735878252166fd
Amosatel <amkimotho68@gmail.com>
2023-03-16 19:01:06 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - this is the entry point where execution begins
+ *
+ * Return: function indicates success of the program
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
+
