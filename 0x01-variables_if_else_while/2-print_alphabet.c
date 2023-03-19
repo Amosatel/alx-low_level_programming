954235f0fd85dda24c0738ad0467b4eaae3327bf
Amosatel <amkimotho68@gmail.com>
2023-03-18 18:21:36 -0700

    call the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - start of the program
+ *
+ * Return: end of the program
+ */
+
+int main(void)
+{
+	char x;
+
+	for (x = 'a' ; x <= 'z' ; x++)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}
