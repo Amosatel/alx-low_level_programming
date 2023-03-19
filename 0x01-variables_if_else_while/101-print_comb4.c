170a8721747d1f7b0e9c6dc58b81093c71ef7f65
Amosatel <amkimotho68@gmail.com>
2023-03-19 11:43:51 -0700

    combo three

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,1 +1,37 @@
+#include <stdio.h>
 
+/**
+ * main - entry point of program and start of execution
+ *
+ * Return: exit of the program after termination
+ */
+
+int main(void)
+{
+	int x;
+	int y;
+	int z;
+
+	for (x = 0 ; x < 10 ; x++)
+	{
+		for (y = 1 ; y < 10 ; y++)
+		{
+			for (z = 2 ; z < 10 ; z++)
+			{
+				if (x < y && y < z)
+				{
+					putchar(x + '0');
+					putchar(y + '0');
+					putchar(z + '0');
+					if (x + y + z != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
