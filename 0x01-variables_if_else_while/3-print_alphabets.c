68d354c79ca1fd5664f704ddb0984c3db963ad20
Amosatel <amkimotho68@gmail.com>
2023-03-20 01:55:34 -0700

    upper and lowercase alphs

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,11 +9,12 @@
 int main(void)
 {
 	char x;
+	char y;
 
 	for (x = 'a' ; x <= 'z' ; x++)
 		putchar(x);
-	for (x = 'A' ; x <= 'Z' ; x++)
-		putchar(x);
+	for (y = 'A' ; y <= 'Z' ; y++)
+		putchar(y);
 	putchar('\n');
 	return (0);
 }
