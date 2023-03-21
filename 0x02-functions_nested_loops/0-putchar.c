4c8e72ea5987ec65e4048d15f9093a780ab7dfb8
Amosatel <amkimotho68@gmail.com>
2023-03-21 05:49:12 -0700

    print _putchar using local var

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - entry point of the program
+ *
+ * _putchar: writes a character to the standard output
+ *
+ * Return: program ends
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,6 @@
-int start(void)
+#ifndef MAIN_H
+#define MAIN_H
 
-	int _putchar(char c)
+int _putchar(char c);
 
-	return(0)
+#endif
