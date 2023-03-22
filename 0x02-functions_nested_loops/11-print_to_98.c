8ef587a70b5e0c3ecfb12044d6da7dce2ba76167
Amosatel <amkimotho68@gmail.com>
2023-03-22 07:13:36 -0700

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints integers  ending at 98
+ *
+ * @n: parameter to be printed at the start
+ *
+ * Return: end of the program
+ */
+
+void print_to_98(int n)
+{
+	int x;
+
+	if (n >= 0 && n <= 98)
+	{
+		for (x = n; x <= 98; x++)
+		{
+			printf("%d", x);
+			if (x != 98)
+				printf(", ");
+		}
+		printf("\n");
+	}
+	else if (n < 0)
+	{
+		for (x = n; x <= 98; x++)
+		{
+			printf("%d", x);
+			if (x != 98)
+				printf(", ");
+		}
+		printf("\n");
+	}
+	else if (n > 98)
+	{
+		for (x = n; x >= 98; x--)
+		{
+			printf("%d", x);
+			if (x != 98)
+				printf(", ");
+		}
+		printf("\n");
+	}
+	else if (n == 98)
+		printf("%d", n);
+}
